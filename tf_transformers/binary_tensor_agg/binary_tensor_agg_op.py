097d91131cec895cd1c08e43985a19858e8c58bd
Ashwini Padhy <ashwini.p@swiggy.in>
2020-07-25 19:09:47 +0530

    removed not needed

--- tf_transformers/binary_tensor_agg/binary_tensor_agg_op.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from ..abstract_operator import Abstract_Operator
-
-class BinaryTensorAggOp(Abstract_Operator):
-
-    Fraction = tf.truediv
--- tf_transformers/binary_tensor_agg/binary_tensor_op.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from ..abstract_operator import Abstract_Operator
-
-class BinaryTensorOp(Abstract_Operator):
-
-    Intersection = tf.sets.intersection
-    Union = tf.sets.union
-    Difference = tf.sets.difference
